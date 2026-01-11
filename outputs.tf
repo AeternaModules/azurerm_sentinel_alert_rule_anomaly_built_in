31acf20ebe1522ed2bdb0a134c8f5751c12417fd
Daniel Allen <dan@aeternagroup.net>
2026-01-11 18:03:21 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "sentinel_alert_rule_anomaly_built_ins" {
+  description = "All sentinel_alert_rule_anomaly_built_in resources"
+  value       = azurerm_sentinel_alert_rule_anomaly_built_in.sentinel_alert_rule_anomaly_built_ins
+}
+
